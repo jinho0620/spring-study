5d058c47880b54686b2a5a25ded12c33108c8c0b
jinho0620 <jinhojo0620@gmail.com>
2025-02-06 15:46:31 +0900

    [Feat] add abstract PaymentService class and two subclasses

---
+++ src/main/java/tobyspring/hellospring/ExRateData.java
@@ -0,0 +1,10 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // Json에 있어도 record에서 안 쓰는 데이터는 무시한다.
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
