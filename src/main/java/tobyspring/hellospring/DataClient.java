e35a0cb6ed20f0b2fdfe25e213ad9768309b6e78
jinho0620 <jinhojo0620@gmail.com>
2025-02-16 13:06:09 +0900

    [Modify] abstract away entitymanager with repository

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -5,6 +5,11 @@ import jakarta.persistence.EntityManagerFactory;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 import tobyspring.hellospring.payment.PaymentService;
@@ -15,18 +20,25 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
-        Order order = new Order("100", BigDecimal.TEN);
-
-        orderRepository.save(order);
-
-        System.out.println(order);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
         try {
-            Order order2 = new Order("100", BigDecimal.ONE);
-            orderRepository.save(order2);
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+
+                orderRepository.save(order);
+
+                System.out.println(order);
+
+                Order order2 = new Order("100", BigDecimal.ONE);
+                orderRepository.save(order2);
+                return null;
+            });
         }
-        catch (ConstraintViolationException e) {
-            System.out.println("주문 번호 충돌을 복구하는 작업");
+        catch (DataIntegrityViolationException e) {
+            System.out.println("주문번호 중복 복구 작업");
         }
+
+
     }
 }
